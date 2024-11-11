1052eea6f72b82ab014b6b0d86adae9496502c8b
sodaaw <ghk132435@naver.com>
2024-11-12 01:14:44 +0900

    css 조금 수정

--- js/pages/login.js
+++ js/pages/login.js
@@ -5,11 +5,8 @@ export function render() {
       <ul class="container">
         <img class="logo" src="./assets/logo.png" />
         <div class="nav">
-          <li>가계부 작성방</li>
-          <li>My 소비내역 보고서</li>
-          <li>My 캐릭터방</li>
-          <li>아이템 구매방</li>
-          <li>금융 뉴스레터</li>
+          <li>home</li>
+          <li>mypage</li>
         </div>
         <div class="btn-group">
           <button class="nav-btn login">로그인</button>
@@ -42,18 +39,13 @@ export function render() {
       </div>
    
       <div class="signupBox">
-        <div class="text-signup">텅장방지기가 처음이신가요?</div>
+        <div class="text-signup">처음 오셨나요?</div>
         <button class="signupBtn">Sign up</button>
       </div>
 
       <div class="sns-login">
         <img class="naver-login" src="./assets/Naver.svg" />
         <img class="kakao-login" src="./assets/Kakao.svg" />
-        <div style="width: 429px; height: 24px; left: 0px; top: 0px; position: absolute">
-          <div style="left: 163px; top: 0px; position: absolute; text-align: center; color: #666666; font-size: 20px; font-family: Inter; font-weight: 500; word-wrap: break-word">SNS 로그인</div>
-          <div style="width: 149px; height: 0px; left: 0px; top: 12px; position: absolute; border: 1px black solid"></div>
-          <div style="width: 156px; height: 0px; left: 273px; top: 12px; position: absolute; border: 1px black solid"></div>
-        </div>
       </div>
     </div>
   `;
