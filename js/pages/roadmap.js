10b9423f44b90ba3e744f424aa24202461326d28
sodaaw <ghk132435@naver.com>
2024-11-20 13:58:04 +0900

    레이아웃 구성 완료

--- js/pages/roadmap.js
+++ js/pages/roadmap.js
@@ -14,14 +14,26 @@ function loadCSS(href) {
   
     // 로드맵 페이지 내용 렌더링
     app.innerHTML = `
-      <div class="roadmap-page">
-        <h1>${selectedMajor} 로드맵 페이지</h1>
-        <div class="roadmap-buttons">
-          <button id="similar-dept-btn" class="roadmap-btn">비슷한 전공 보기</button>
-          <button id="roadmap-img-btn" class="roadmap-btn">로드맵 이미지 보기</button>
-          <button id="dept-link-btn" class="roadmap-btn">학과 홈페이지 이동</button>
+        <header class="roadmap-header">
+            <div class="header-left">
+                <id="logo-button" img src="images/logo.png" alt="앱 로고" class="app-logo"/> <!-- 앱 로고 추가 -->
+            </div>
+            <div class="header-right">
+                <button id="home-btn" class="home-btn">Home</button>
+                <button id="logout-btn" class="logout-btn">Logout</button>
+            </div>
+        </header>
+        <div class="roadmap-page">
+            <h1>${selectedMajor} 로드맵 페이지</h1>
+            <div class="roadmap-buttons">
+            <button id="similar-dept-btn" class="roadmap-btn">비슷한 전공 보기</button>
+            <button id="roadmap-img-btn" class="roadmap-btn">로드맵 이미지 보기</button>
+            <button id="dept-link-btn" class="roadmap-btn">학과 홈페이지 이동</button>
+            </div>
+            <div class="back-button-container">
+                <button id="back-btn" class="roadmap-btn back-btn">돌아가기</button>
+            </div>
         </div>
-      </div>
     `;
   
     // 비슷한 전공 보기 버튼 이벤트
